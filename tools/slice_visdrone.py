5355e2717d33cde5b9209d6bc6318ec74b96180c
Hao Zhang <haozhang@njust.edu.cn>
2025-06-12 16:38:59 +0800

    Add files via upload

---
+++ tools/slice_visdrone.py
@@ -0,0 +1,29 @@
+import fire
+from slice_coco import slice
+from tqdm import tqdm
+
+SLICE_SIZE_LIST = [480, 640]
+OVERLAP_RATIO_LIST = [0.15]
+IGNORE_NEGATIVE_SAMPLES = False
+
+
+def slice_visdrone(image_dir: str, dataset_json_path: str, output_dir: str):
+    total_run = len(SLICE_SIZE_LIST) * len(OVERLAP_RATIO_LIST)
+    current_run = 1
+    for slice_size in SLICE_SIZE_LIST:
+        for overlap_ratio in OVERLAP_RATIO_LIST:
+            tqdm.write(
+                f"{current_run} of {total_run}: slicing for slice_size={slice_size}, overlap_ratio={overlap_ratio}"
+            )
+            slice(
+                image_dir=image_dir,
+                dataset_json_path=dataset_json_path,
+                output_dir=output_dir,
+                slice_size=slice_size,
+                overlap_ratio=overlap_ratio,
+            )
+            current_run += 1
+
+
+if __name__ == "__main__":
+    fire.Fire(slice_visdrone('D:/Project/datasets/VisDrone/VisDrone2019-DET-train/','D:/Project/datasets/VisDrone/VisDrone2019-DET-train/coco.json','D:/Project/datasets/VisDrone/ASM_512/output_train'))
